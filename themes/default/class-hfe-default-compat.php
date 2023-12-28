12dcdeacfe2c313042571d3b40b32fe4abb89ad5
Aref Alikhani <aalikhanid@gmail.com>
2023-12-28 10:48:10 +0330

    Added 404 filter and action to override the default template

--- themes/default/class-hfe-default-compat.php
+++ themes/default/class-hfe-default-compat.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * HFE_Default_Compat setup
  *
@@ -10,46 +11,56 @@ namespace HFE\Themes;
 /**
  * Astra theme compatibility.
  */
-class HFE_Default_Compat {
+class HFE_Default_Compat
+{
 
 	/**
 	 *  Initiator
 	 */
-	public function __construct() {
-		add_action( 'wp', [ $this, 'hooks' ] );
+	public function __construct()
+	{
+		add_action('wp', [$this, 'hooks']);
 	}
 
 	/**
 	 * Run all the Actions / Filters.
 	 */
-	public function hooks() {
-		if ( hfe_header_enabled() ) {
+	public function hooks()
+	{
+		if (hfe_header_enabled()) {
 			// Replace header.php template.
-			add_action( 'get_header', [ $this, 'override_header' ] );
+			add_action('get_header', [$this, 'override_header']);
 
 			// Display HFE's header in the replaced header.
-			add_action( 'hfe_header', 'hfe_render_header' );
+			add_action('hfe_header', 'hfe_render_header');
 		}
 
-		if ( hfe_footer_enabled() || hfe_is_before_footer_enabled() ) {
+		if (hfe_footer_enabled() || hfe_is_before_footer_enabled()) {
 			// Replace footer.php template.
-			add_action( 'get_footer', [ $this, 'override_footer' ] );
+			add_action('get_footer', [$this, 'override_footer']);
 		}
 
-		if ( hfe_footer_enabled() ) {
+		if (hfe_footer_enabled()) {
 			// Display HFE's footer in the replaced header.
-			add_action( 'hfe_footer', 'hfe_render_footer' );
+			add_action('hfe_footer', 'hfe_render_footer');
 		}
 
-		if ( hfe_is_before_footer_enabled() ) {
-			add_action( 'hfe_footer_before', [ 'Header_Footer_Elementor', 'get_before_footer_content' ] );
+		if (hfe_is_before_footer_enabled()) {
+			add_action('hfe_footer_before', ['Header_Footer_Elementor', 'get_before_footer_content']);
 		}
 
 		if (hfe_single_post_enabled()) {
-			// Replace single.php template.
-			add_filter( 'template_include',[$this, 'override_single_post_template']  );
+			// Replace template.
+			add_filter('template_include', [$this, 'override_template']);
 			// Display HFE's single post instead of single.php template
 			add_action('hfe_single_post', 'hfe_render_single_post');
+		}
+
+		if (hfe_404_enabled()) {
+			// Replace template.
+			add_filter('template_include', [$this, 'override_template']);
+			// Display HFE's 404 instead of 404.php template
+			add_action('hfe_404', 'hfe_render_404');
 		}
 	}
 
@@ -60,14 +71,15 @@ class HFE_Default_Compat {
 	 *
 	 * @return void
 	 */
-	public function override_header() {
+	public function override_header()
+	{
 		require HFE_DIR . 'themes/default/hfe-header.php';
 		$templates   = [];
 		$templates[] = 'header.php';
 		// Avoid running wp_head hooks again.
-		remove_all_actions( 'wp_head' );
+		remove_all_actions('wp_head');
 		ob_start();
-		locate_template( $templates, true );
+		locate_template($templates, true);
 		ob_get_clean();
 	}
 
@@ -78,14 +90,15 @@ class HFE_Default_Compat {
 	 *
 	 * @return void
 	 */
-	public function override_footer() {
+	public function override_footer()
+	{
 		require HFE_DIR . 'themes/default/hfe-footer.php';
 		$templates   = [];
 		$templates[] = 'footer.php';
 		// Avoid running wp_footer hooks again.
-		remove_all_actions( 'wp_footer' );
+		remove_all_actions('wp_footer');
 		ob_start();
-		locate_template( $templates, true );
+		locate_template($templates, true);
 		ob_get_clean();
 	}
 
@@ -95,18 +108,20 @@ class HFE_Default_Compat {
 	 *
 	 * @return void
 	 */
-	function override_single_post_template( $template ) {
+	function override_template($template)
+	{
 		// Check if this is a single post request
-		if ( is_single() ) {
-			$custom_template = plugin_dir_path( __FILE__ ) . 'hfe-single-post.php';
-			// Check if the custom template file exists
-			if ( file_exists( $custom_template ) ) {
-				return $custom_template;
-			}
+		if (is_single()) {
+			$custom_template = plugin_dir_path(__FILE__) . 'hfe-single-post.php';
+		} elseif (is_404()) {
+			$custom_template = plugin_dir_path(__FILE__) . 'hfe-404.php';
+		}
+		// Check if the custom template file exists
+		if (file_exists($custom_template)) {
+			return $custom_template;
 		}
 		return $template;
 	}
-
 }
 
 new HFE_Default_Compat();
