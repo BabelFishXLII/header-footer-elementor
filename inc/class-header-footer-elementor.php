3522cd4c904f37f5eaca65e47ae35f7a994e4767
Aref Alikhani <aalikhanid@gmail.com>
2023-12-31 18:37:07 +0330

    get archive content and template type settings added

--- inc/class-header-footer-elementor.php
+++ inc/class-header-footer-elementor.php
@@ -526,6 +526,15 @@ class Header_Footer_Elementor
 	}
 
 	/**
+	 * Prints the archive content.
+	 */
+	public static function get_archive_content()
+	{
+		$archive_content = self::$elementor_instance->frontend->get_builder_content_for_display(get_hfe_archive_id());
+		echo $archive_content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+
+	/**
 	 * Prints the 404 content.
 	 */
 	public static function get_404_content()
@@ -588,7 +597,8 @@ class Header_Footer_Elementor
 			'type_before_footer' == $setting  ||
 			'type_single_post' == $setting ||
 			'type_404' == $setting ||
-			'type_single_page' == $setting
+			'type_single_page' == $setting ||
+			'type_archive' == $setting
 		) {
 			$templates = self::get_template_id($setting);
 
