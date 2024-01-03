3e64401f05f2d30ecdc62136467ebf90388cbcbc
Aref Alikhani <aalikhanid@gmail.com>
2024-01-04 00:22:20 +0330

    hef-search.php created

---
+++ themes/default/hfe-search.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Custom Single Post Template
+ *
+ * This template is used to display single post content.
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Include the theme header
+get_header();
+
+// Start the WordPress loop
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+		do_action( 'hfe_search' );
+    endwhile;
+endif;
+
+// Include the theme footer
+get_footer();
